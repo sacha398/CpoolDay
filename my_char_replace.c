f1ba1ca2074b74dd702de0b49e930fa5eeb55cbb
sacha398 <52128884+sacha398@users.noreply.github.com>
2024-04-17 10:31:19 +0000

    charReplace

---
+++ my_char_replace.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_char(char c);
+
+char *my_replace_char(char *origin, char toFind, char toReplace){
+    int j = 0;
+    while(origin[j] != '\0'){
+        j++;
+    }
+    for (int i = 0; i < j; i++)
+    {
+       if(origin[i] == toFind){
+            origin[i] = toReplace;
+       }
+    }
+
+    return origin;
+
+}
