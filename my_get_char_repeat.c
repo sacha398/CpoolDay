f450e0f5c21bd2b222a2e36d9267b25fde6188e4
sacha398 <52128884+sacha398@users.noreply.github.com>
2024-04-17 11:35:19 +0000

    getChar

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int my_get_char_repeat(char to_find, const char *to_search){
+    if(to_search == NULL){
+        return 0;
+    }
     int a = 0, cpt = 0;
     while(to_search[a] != '\0'){
         a++;
